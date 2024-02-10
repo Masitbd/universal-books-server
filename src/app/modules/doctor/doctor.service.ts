903504405fcad773b015ec01755a936f9310245e
emonibnsalim <emonibnsali@gmail.com>
2024-02-10 19:21:05 +0600

    doctor section  complete

---
+++ src/app/modules/doctor/doctor.service.ts
@@ -0,0 +1,53 @@
+import httpStatus from 'http-status';
+import ApiError from '../../../errors/ApiError';
+import { IDoctor } from './doctor.interface';
+import { Doctor } from './doctor.model';
+
+const createDoctor = async (payload: IDoctor): Promise<IDoctor> => {
+  payload.defaultImage = '';
+  const result = await Doctor.create(payload);
+  return result;
+};
+
+const updateDoctor = async (
+  id: string,
+  payload: Partial<IDoctor>
+): Promise<IDoctor | null> => {
+  const result = await Doctor.findOneAndUpdate(
+    {
+      _id: id,
+    },
+    payload,
+    {
+      new: true,
+    }
+  );
+  return result;
+};
+
+const deleteDoctor = async (id: string) => {
+  const result = await Doctor.findOneAndDelete({ _id: id });
+  return result;
+};
+
+const getAllDoctor = async (): Promise<IDoctor[] | null> => {
+  const result = await Doctor.find();
+  return result;
+};
+
+const getSingleDoctor = async (id: string): Promise<IDoctor | null> => {
+  const result = await Doctor.findOne({ _id: id });
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Doctor not found');
+  }
+
+  return result;
+};
+
+export const DoctorServices = {
+  createDoctor,
+  updateDoctor,
+  deleteDoctor,
+  getAllDoctor,
+  getSingleDoctor,
+};
