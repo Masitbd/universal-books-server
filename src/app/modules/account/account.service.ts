379f904414ce72578805062dce8862bcd2d4811f
maruf9270 <maruf9270@gmail.com>
2024-02-28 23:11:32 +0600

    Added transaction functionality to the backend

--- src/app/modules/account/account.service.ts
+++ src/app/modules/account/account.service.ts
@@ -1,28 +1,52 @@
+import httpStatus from 'http-status';
+import mongoose from 'mongoose';
+import ApiError from '../../../errors/ApiError';
 import { ITransaction } from '../transaction/transaction.interface';
 import { IAccount } from './account.interface';
 import { Account } from './account.model';
-
 const postNewAccount = async (data: IAccount) => {
   const result = await Account.create(data);
   return result;
 };
 
 const patchAccount = async (data: ITransaction) => {
-  const transBalanceType = data.transactionType;
-  const account: IAccount | null = await Account.findOne({ uuid: data.uuid });
-  const userBalanceType = account?.balanceType;
-  if (transBalanceType && account) {
-    if (userBalanceType === transBalanceType) {
-      const amount = Number(account?.balance) + Number(data.amount);
-      account.balance = amount;
-      account.save();
-    } else {
-      const amount = Number(account?.balance) - Number(data.amount);
-      account.balance = amount;
-      account.save();
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const transBalanceType = data.transactionType;
+    const account: IAccount | null = await Account.findOne({ uuid: data.uuid });
+    if (!account) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Account not found');
     }
+    const userBalanceType = account?.balanceType;
+    if (transBalanceType && account) {
+      if (userBalanceType === transBalanceType) {
+        const amount = Number(account?.balance) + Number(data.amount);
+        account.balance = amount;
+        account.save();
+      } else {
+        if (account.balance < data.amount) {
+          throw new ApiError(
+            httpStatus.NOT_ACCEPTABLE,
+            'There is not enough balance in this account for transaciton'
+          );
+        }
+        const amount = Number(account?.balance) - Number(data.amount);
+        account.balance = amount;
+        account.save();
+        await session.commitTransaction();
+        session.endSession();
+        return account;
+      }
+    }
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    throw new ApiError(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      'Internal server error. Try again later.'
+    );
   }
-  return account;
 };
 
 const fetchSingle = async (params: string) => {
