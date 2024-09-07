6ebb13d1e609e8249874c7398eb447121f0dc23a
sarker24 <sarker24@gmail.com>
2024-09-07 09:45:52 +0200

    Add new machine

--- index.js
+++ index.js
@@ -234,7 +234,7 @@ app.patch('/book/:id', async (req, res) => {
 run().catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World run from private view')
+  res.send('Hello World run from new machine')
 })
 
 const server = app.listen(port, '0.0.0.0', () => {
